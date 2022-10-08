e94d3f5a9b11401a06bfc3960cc6fd62dbc6a17b
zahoor ahmad <86906265+Zahoor58@users.noreply.github.com>
2022-10-09 00:45:55 +0500

    class and function base components

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+// import { Header } from './components/header';
+import Header from './components/header';
+import Footer from "./components/footer.js";
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        Header / >
+        <
+        p > main content < /p>  <
+        Footer / > < /
+        div >
+    );
+}
+
+export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+    render() {
+        return <h1 > This is our Footer < /h1>
+    }
+
+}
+export default Footer;
