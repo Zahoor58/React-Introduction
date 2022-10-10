e4ca55ff11fb1dc441059a00f11f88ffb87eeee7
zahoor ahmad <86906265+Zahoor58@users.noreply.github.com>
2022-10-11 02:02:42 +0500

    State is mutable but probs is immutabele

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,8 +5,12 @@ class Footer extends Component {
     // createAlert() {
     //     alert('Hello, you clicked me');
     // }
-    changed() {
-        console.log('change');
+    state = {
+        name: "Zahoor Shad"
+    }
+    changed = (evt) => {
+        this.setState({ name: evt.target.value })
+        console.log('changed', evt.target.value);
     }
     render() {
 
@@ -14,8 +18,7 @@ class Footer extends Component {
                 /* <
                             h1 onClick = { this.createAlert } > { this.props.trademark } < /h1 >  */
             } <
-            h1 onClick = { this.props.myalert } > { this.props.trademark } < /h1 >  <
-        input onChange = { this.changed }
+            h1 onClick = { this.props.myalert } > { this.props.trademark } < /h1 >  <input value={this.state.name} onChange = { this.changed }
         type = 'text' / >
             <
             /
