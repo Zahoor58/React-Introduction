f8dc434349159c7fcde9f0dd54555815c5135376
zahoor ahmad <86906265+Zahoor58@users.noreply.github.com>
2022-10-12 00:53:03 +0500

    Explored diff Conditional display method in react

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ function createAlert() {
     alert('Hello, you clicked me');
 }
 
+function ShowMessage(props) {
+    if (props.toShow) {
+        return <h2 > My message < /h2>
+    } else {
+        return <h2 > Forbiden < /h2>
+    }
+
+}
+
 function App() {
     return ( <
         div className = "App" >
@@ -17,7 +26,10 @@ function App() {
         p > main content < /p>  <
         Footer trademark = "Page by Zahoor"
         myalert = { createAlert }
-        />< /
+        />   <
+        // ShowMessage toShow = { true }
+        // / > <
+        /
         div >
     );
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,9 @@ class Footer extends Component {
     //     alert('Hello, you clicked me');
     // }
     state = {
-        name: "Zahoor Shad"
+        name: "Zahoor Shad",
+        age: 35,
+        isLogin: true
     }
     changed = (evt) => {
         this.setState({ name: evt.target.value })
@@ -15,13 +17,29 @@ class Footer extends Component {
     render() {
 
         return <div > {
-                /* <
-                            h1 onClick = { this.createAlert } > { this.props.trademark } < /h1 >  */
-            } <
-            h1 onClick = { this.props.myalert } > { this.props.trademark } < /h1 >  <input value={this.state.name} onChange = { this.changed }
-        type = 'text' / >
-            <
-            /
+            /* <
+                        h1 onClick = { this.createAlert } > { this.props.trademark } < /h1 >  */
+        } {
+            // this.state.age === 35 ? "Yes" : "No"
+            this.state.isLogin ? ( <
+                React.Fragment > <
+                h1 onClick = { this.props.myalert } > { this.props.trademark } < /h1 >  <input value={this.state.name} onChange = { this.changed }
+                type = 'text' / >
+                <
+                /React.Fragment>
+            ) : ( <
+                React.Fragment > <
+                h1 > you cannot se e this content < /h1 >  <
+                h2 > You must be login < /h2>  < /
+                React.Fragment >
+            )
+
+        }
+
+
+
+        <
+        /
         div >
     }
 
