3fcfbae7944c68370321668542e74d97222a44d8
zahoor ahmad <86906265+Zahoor58@users.noreply.github.com>
2022-10-12 01:30:21 +0500

    Explored loop and key in react.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 // import { Header } from './components/header';
-import Header from './components/header';
+import Header from "./components/header";
 import Footer from "./components/footer.js";
 
 function createAlert() {
-    alert('Hello, you clicked me');
+  alert("Hello, you clicked me");
 }
 
 function ShowMessage(props) {
-    if (props.toShow) {
-        return <h2 > My message < /h2>
-    } else {
-        return <h2 > Forbiden < /h2>
-    }
-
+  if (props.toShow) {
+    return <h2> My message </h2>;
+  } else {
+    return <h2> Forbiden </h2>;
+  }
 }
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        Header info = "This is my message" / >
-        <
-        p > main content < /p>  <
-        Footer trademark = "Page by Zahoor"
-        myalert = { createAlert }
-        />   <
-        // ShowMessage toShow = { true }
-        // / > <
-        /
-        div >
-    );
+  return (
+    <div className="App">
+      <Header info="This is my message" />
+      <p> main content </p>{" "}
+      <Footer trademark="Page by Zahoor" myalert={createAlert} />{" "}
+      {/* ShowMessage toShow = { true }
+       / >  */}
+    </div>
+  );
 }
 
 export default App;
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,47 +1,35 @@
 import React, { Component } from "react";
 
 class Footer extends Component {
-    // inside class
-    // createAlert() {
-    //     alert('Hello, you clicked me');
-    // }
-    state = {
-        name: "Zahoor Shad",
-        age: 35,
-        isLogin: true
-    }
-    changed = (evt) => {
-        this.setState({ name: evt.target.value })
-        console.log('changed', evt.target.value);
-    }
-    render() {
-
-        return <div > {
-            /* <
-                        h1 onClick = { this.createAlert } > { this.props.trademark } < /h1 >  */
-        } {
-            // this.state.age === 35 ? "Yes" : "No"
-            this.state.isLogin ? ( <
-                React.Fragment > <
-                h1 onClick = { this.props.myalert } > { this.props.trademark } < /h1 >  <input value={this.state.name} onChange = { this.changed }
-                type = 'text' / >
-                <
-                /React.Fragment>
-            ) : ( <
-                React.Fragment > <
-                h1 > you cannot se e this content < /h1 >  <
-                h2 > You must be login < /h2>  < /
-                React.Fragment >
-            )
-
-        }
-
-
-
-        <
-        /
-        div >
-    }
-
+  // inside class
+  // createAlert() {
+  //     alert('Hello, you clicked me');
+  // }
+  state = {
+    name: "Zahoor Shad",
+    age: 35,
+    isLogin: true,
+  };
+  changed = (evt) => {
+    this.setState({ name: evt.target.value });
+    console.log("changed", evt.target.value);
+  };
+  render() {
+    const animals=['cat','dog','horse']
+    return (
+      <div>
+            {
+                animals.map(animal=>{
+                    return (
+                    <div key={animal}>
+                        <h1>{animal}</h1>
+                        <h1>{animal}</h1>
+                    </div>
+                    )
+                })
+            }
+      </div>
+    );
+  }
 }
 export default Footer;
