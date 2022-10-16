0f40096e7ce43939775d667f981c88ed47473a93
zahoor ahmad <86906265+Zahoor58@users.noreply.github.com>
2022-10-16 14:35:33 +0500

    Explored online and normal styling in react.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 // import { Header } from './components/header';
 import Header from "./components/header";
 import Footer from "./components/footer.js";
-
+import styled, { css } from 'styled-components'
 function createAlert() {
   alert("Hello, you clicked me");
 }
@@ -15,15 +15,25 @@ function ShowMessage(props) {
     return <h2> Forbiden </h2>;
   }
 }
-
+const pStyle = {
+  fontSize: "2em",
+  color:'red',
+};
+ const Paragraph=styled.p`
+ font-size:3em;
+ color:green;
+ `;
 function App() {
   return (
     <div className="App">
       <Header info="This is my message" />
-      <p> main content </p>
-      <Footer trademark="Page by Zahoor" myalert={createAlert} />
+      <p style={pStyle}> main content </p>{" "}
+      <Paragraph>
+        New style
+      </Paragraph>
+      <Footer trademark="Page by Zahoor" myalert={createAlert} />{" "}
       {/* ShowMessage toShow = { true }
-       / >  */}
+                   / >  */}{" "}
     </div>
   );
 }
